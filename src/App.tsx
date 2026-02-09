61c9369c05dd066b4a0db7922f054f3de01a49ba
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2026-02-09 13:06:44 +0000

    Handling Loading & Error States

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, type ReactNode } from 'react'
 import type { BlogPost } from './components/BlogPosts.tsx'
 import { get } from './util/http.ts'
 import BlogPosts from './components/BlogPosts.tsx'
 import fetchingImg from './assets/data-fetching.png'
+import ErrorMessage from './components/ErrorMessage.tsx'
 
 type RawDataBlogPost = {
   id: number
@@ -12,25 +13,50 @@ type RawDataBlogPost = {
 }
 
 function App() {
-  const [fetchedPost, setFetchedPost] = useState<BlogPost[]>([])
+  const [fetchedPost, setFetchedPost] = useState<BlogPost[]>()
+  const [isFetching, setIsFetching] = useState(false)
+  const [error, setError] = useState<string>()
 
   useEffect(() => {
     async function fetchPosts() {
-      const data = await get<RawDataBlogPost[]>(
-        'https://jsonplaceholder.typicode.com/posts',
-      )
+      setIsFetching(true)
+      try {
+        const data = await get<RawDataBlogPost[]>(
+          'https://jsonplaceholder.typicode.com/posts',
+        )
 
-      const blogPosts: BlogPost[] = data.map((rawPost) => ({
-        id: rawPost.id,
-        title: rawPost.title,
-        text: rawPost.body,
-      }))
+        const blogPosts: BlogPost[] = data.map((rawPost) => ({
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body,
+        }))
 
-      setFetchedPost(blogPosts)
+        setFetchedPost(blogPosts)
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message)
+        }
+      }
+
+      setIsFetching(false)
     }
 
     fetchPosts()
   }, [])
+
+  let content: ReactNode
+
+  if (error) {
+    content = <ErrorMessage text={error} />
+  }
+
+  if (fetchedPost) {
+    content = <BlogPosts posts={fetchedPost} />
+  }
+
+  if (isFetching) {
+    content = <p id='loading-fallback'>Fetching post...</p>
+  }
 
   return (
     <main>
@@ -38,7 +64,7 @@ function App() {
         src={fetchingImg}
         alt='An abstract image depicting a data fetching process'
       />
-      <BlogPosts posts={fetchedPost} />
+      {content}
     </main>
   )
 }
